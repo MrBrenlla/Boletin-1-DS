d31c905fbf4d6bac1b3ad0b7d8437bc33602019b
MrBrenlla <47985849+MrBrenlla@users.noreply.github.com>
2019-10-03 16:57:22 +0200

    Add files via upload

---
+++ Color.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e4;
+
+/**
+ *
+ * @author Brais
+ */
+public enum Color{
+    ORO("O"),BRONCE("B"),ORO_PLATA("OP") ;
+    
+    private final String inicial;
+    
+    Color(String color){
+        this.inicial=color; 
+    }
+}
